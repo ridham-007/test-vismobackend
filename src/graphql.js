c72169db9fea96c3f49b3f234b5e95640aae885b
ridham-007 <ridhamavaiya1999@gmail.com>
2023-10-23 15:25:38 +0530

    test render.yaml

--- src/graphql.js
+++ src/graphql.js
@@ -3,7 +3,7 @@
 const cutsomApi = require('./api/custom-api/graphql')
 const extensions = [cutsomApi]
 
-//self note for understanding
+//self note for understanding and anyone can update that regarding his knowledge
 
 module.exports = (strapi) => {
     const extensionService = strapi.plugin('graphql').service('extension')
